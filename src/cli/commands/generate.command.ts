470c17e3b7af0725dd92cf1cc0b4a765413def5b
senyapekus <ksenya.pshenichnikova03@gmail.com>
2024-10-06 19:25:52 +0500

    Приложение для терминала. Часть 2

---
+++ src/cli/commands/generate.command.ts
@@ -0,0 +1,33 @@
+import { Command } from './command.interface.js';
+import { MockServerData } from '../../shared/types/mock-server-data.type.js';
+import got from 'got';
+import TSVOfferGenerator from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import TsvFileWriter from '../../shared/libs/file-writer/tsv-file-writer.js';
+
+export class GenerateCommand implements Command {
+  private initialData!: MockServerData;
+  public getName(): string {
+    return '--generate';
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      console.log(`Can't fetch data from ${url}`);
+      return;
+    }
+
+    const offerGeneratorString = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TsvFileWriter(filepath);
+
+    for (let i = 0; i < offerCount; i++) {
+      await tsvFileWriter.write(offerGeneratorString.generate());
+    }
+
+    console.log(`File ${filepath} was created`);
+  }
+}
